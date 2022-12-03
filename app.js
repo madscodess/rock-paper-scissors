d752d42ddd2b2d1ff6c98758e253c43a830ebde3
madscodess <32851804+madscodess@users.noreply.github.com>
2022-12-03 12:33:40 +0000

    added game conditions

--- app.js
+++ app.js
@@ -10,13 +10,30 @@ switch(randomChoice) {
     case 2:
         return 'scissors';
 };
-}   
+} 
+
+function playRound(playerSelection, computerSelection) {
+   if (playerSelection === computerSelection) {
+      return `It's a tie! you both picked ${playerSelection}`;
+   } else if (playerSelection === "rock" && computerSelection === "scissors") {
+      return "You win! Rock beats Scissors";
+   } else if (playerSelection === "paper" && computerSelection === "rock") {
+      return "You win! Paper beats Rock";
+   } else if (playerSelection === "scissors" && computerSelection === "paper") {
+      return "You win! Scissors beats Paper";
+   } else {
+      return `You lose! ${computerSelection} beats ${playerSelection}`;
+   }
+}
+
 const computerSelection = getComputerChoice(0, 2)
 console.log (computerSelection);
-
-const playerSelection = prompt("You start the game, pick 'Rock, Paper or Scissors'").toLowerCase();
+const playerSelection = prompt("Start the game by picking among 'Rock, Paper, Scissors'").toLowerCase();
 if (playerSelection !== 'rock' &&
      playerSelection !== 'paper' && 
      playerSelection !== 'scissors') {
-    const invalidChoice = prompt("Invalid Choice - You start the game, pick 'Rock, Paper or Scissors'").toLowerCase();
-}
+     const invalidChoice = alert("Invalid Choice")
+     }
+
+console.log(playRound(playerSelection, computerSelection))
+     
