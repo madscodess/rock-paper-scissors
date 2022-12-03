687ca257d230ce292b8cff296669d9e3fb95df4f
madscodess <32851804+madscodess@users.noreply.github.com>
2022-12-03 12:08:27 +0000

    connecting files

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("hello")
+
+
+function getComputerChoice(min, max) { // min and max included 
+        const randomChoice = Math.floor(Math.random() * (max - min + 1) + min)
+switch(randomChoice) {
+    case 0:
+        return 'rock';
+    case 1:
+        return 'paper';
+    case 2:
+        return 'scissors';
+};
+}   
+const computerSelection = getComputerChoice(0, 2)
+console.log (computerSelection);
+
+
